dc947acb4a5c1a81437ca351d153d8a2b605926f
JuanCarlosTRW <2464712@cmontmorency.qc.ca>
2026-02-17 21:52:26 -0500

    Make Unicorn Studio hero scene full screen and responsive on all devices

--- src/components/case-studies/CaseStudiesHero.tsx
+++ src/components/case-studies/CaseStudiesHero.tsx
@@ -2,12 +2,15 @@ import UnicornScene from "unicornstudio-react/next";
 
 export default function CaseStudiesHero() {
   return (
-    <div className="w-full flex justify-center items-center mb-12">
+    <div
+      className="w-screen h-[60vh] min-h-[320px] max-h-[100vh] relative left-1/2 right-1/2 -mx-[50vw] overflow-hidden mb-12"
+      style={{ minWidth: '100vw' }}
+    >
       <UnicornScene
         projectId="hU6U4mKRHf6q7lApTLwx"
         sdkUrl="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v2.0.5/dist/unicornStudio.umd.js"
         width="100%"
-        height="400px"
+        height="100%"
         production={true}
         scale={1}
         dpi={1.5}
