39e37df630f3e959cebf47e19e342876a5b6127b
JuanCarlosTRW <2464712@cmontmorency.qc.ca>
2026-02-18 16:12:26 -0500

    fix: update navbar logo image and speed up transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
       colors={["transparent", "transparent"]}
       isFixed={true}
       position="right"
-      logoUrl="https://static.wixstatic.com/media/62f926_145f629a54634971b81de3e9565c7928~mv2.png"
+  logoUrl="https://static.wixstatic.com/media/62f926_ec220470b8404c1c919ff842815eb9bd~mv2.png"
     />
   );
 }
